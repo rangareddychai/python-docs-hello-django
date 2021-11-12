62e1e461dad342638490df1deff364a9626f6893
rangareddychai <93305179+rangareddychai@users.noreply.github.com>
2021-11-12 11:42:50 +0530

    Update views.py

--- hello/views.py
+++ hello/views.py
@@ -2,4 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def hello(request):
-    return HttpResponse("Congragulations to all")
+    return HttpResponse("Congragulations to one and all")
